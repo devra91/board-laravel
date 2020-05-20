60bbd57d8add534c370866ff7caeaca33338d51a
Keesung CHOI <keesung@Keesungui-MacBookPro.local>
2020-05-20 15:04:42 +0900

    first commit

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Board;
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Board::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+        $data = $request->validate([
+            'sTitle' => 'required',
+            'sContext' => 'required',
+            // 'nUserID' => 'required',
+        ]);
+        $board = Board::create($data);
+
+        return response($board, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Board $board)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Board $board)
+    {
+        $data = $request->validate([
+            'sTitle' => 'required',
+            'sContext' => 'required',
+            'nUserID' => 'required',
+        ]);
+        $board->update($data);
+
+        return response($board, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Board  $board
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Board $board)
+    {
+        $board->delete();
+
+        return response('Deleted item', 200);
+    }
+}
---
+++ database/migrations/2020_05_19_153441_create_boards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sTitle');
+            $table->string('sContext', 1000);
+            // $table->string('image');
+            $table->timestamps();
+            // $table->integer('nUserID')->unsigned()->nullable();
+
+            // $table->foreign('nUserID')->references('id')->on('users');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
